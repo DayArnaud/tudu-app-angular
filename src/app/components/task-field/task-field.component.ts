5326198e4c6f1391d4c9f777ecf682801d66481a
DayaneArnaud <dayanearnaud@gmail.com>
2023-07-25 20:57:06 -0300

    feat: planner date

--- src/app/components/task-field/task-field.component.ts
+++ src/app/components/task-field/task-field.component.ts
@@ -1,16 +1,69 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+// import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+// @Component({
+//     selector: 'app-task-field',
+//     templateUrl: './task-field.component.html',
+//     styleUrls: ['./task-field.component.css']
+// })
+// export class TaskFieldComponent {
+//     @Input() showCheckbox: boolean = true;
+//     @Input() showDate: boolean = false;
+//     @Input() task: any;
+//     @Output() checkboxClick = new EventEmitter<any>();
+
+
+//     onCheckboxClick() {
+//         this.checkboxClick.emit(this.task);
+//     }
+
+//     getCategoryColor(category: number) {
+//         switch (category) {
+//             case 1:
+//                 return 'var(--salmon)';
+//             case 2:
+//                 return 'var(--yellow)';
+//             case 3:
+//                 return 'var(--blue)';
+//             default:
+//                 return 'var(--dark-blue)';
+//         }
+//     }
+//     getStatus(status: number) {
+//         if (status === 2) {
+//             return 'var(--gray)';
+//         }
+//         return;
+//     }
+// }
+
+import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
+import { TuduService } from 'src/app/services/tudu.service';
 
 @Component({
     selector: 'app-task-field',
     templateUrl: './task-field.component.html',
     styleUrls: ['./task-field.component.css']
 })
-export class TaskFieldComponent {
+export class TaskFieldComponent implements OnInit {
     @Input() showCheckbox: boolean = true;
     @Input() showDate: boolean = false;
     @Input() task: any;
     @Output() checkboxClick = new EventEmitter<any>();
 
+    public taskDetail: any = {};
+
+    constructor(private tuduService: TuduService) { }
+
+    ngOnInit(): void {
+        this.loadTaskDetails();
+    }
+
+    loadTaskDetails() {
+        const taskId = this.task._id.$oid;
+        this.tuduService.listTaskById(taskId).subscribe(task => {
+            this.taskDetail = task;
+        });
+    }
 
     onCheckboxClick() {
         this.checkboxClick.emit(this.task);
@@ -28,6 +81,7 @@ export class TaskFieldComponent {
                 return 'var(--dark-blue)';
         }
     }
+
     getStatus(status: number) {
         if (status === 2) {
             return 'var(--gray)';
